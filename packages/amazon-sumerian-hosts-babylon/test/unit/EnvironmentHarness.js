1fa95917069f5e54591e2e44eeb81d9072bce2eb
brhook-aws <brhook-aws@users.noreply.github.com>
2022-04-20 20:44:14 +0000

    deploy: 3528e58f47693e441abf4bc80a0a508146cc9dcd

--- packages/amazon-sumerian-hosts-babylon/test/unit/EnvironmentHarness.js
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-import describeBabylonHost from './BabylonHarness';
-
-export default function describeHostEnvironment(description, fn) {
-  describeBabylonHost(description, fn);
-}
--- packages/amazon-sumerian-hosts-three/test/unit/EnvironmentHarness.js
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-import describeThreeHost from './ThreeHarness';
-
-export default function describeHostEnvironment(description, fn) {
-  describeThreeHost(description, fn);
-}
